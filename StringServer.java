9b8896c85599712c1dd85273e4c13d68a91b6276
Ryan Ryu <132784680+ryanryucode@users.noreply.github.com>
2023-10-22 23:19:09 -0700

    Update and rename StringServer to StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -4,18 +4,18 @@ import java.net.URI;
 class Handler implements URLHandler {
     // Add on strings as the user inputs the string
     int num = 0;
-    string newString;
+    String newString;
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format(num, ". ", newString);
+            return String.format("%d. %s", num, newString);
         } 
         else {
             if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
                     num += 1;
-                    return String.format(num, ". ", newString);
+                    return String.format("%d. %s", num, newString);
                 }
             }
             return "404 Not Found!";
